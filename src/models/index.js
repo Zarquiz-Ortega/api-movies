777eee9c5d65f81b023189126f93412a83be27be
Zarquiz Ortega <vian0zarco@gmail.com>
2024-06-11 19:22:38 -0600

    relaciones

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,18 @@
-
-
-//Todos Relaciones de uno a muchos (N-M)
-
 const Actor = require("./Actor");
 const Director = require("./Director");
 const Genre = require("./Genre");
 const Movie = require("./Movie");
 
+
+//Todos Relaciones de uno a muchos (N-M)
+
+
 //Todos Relaciones de muchos a muchos (M-M)
 Movie.belongsToMany(Actor, {through: 'movieActor'})
+Actor.belongsToMany(Movie, {through: 'actorMovie'})
+
 Movie.belongsToMany(Director,{through: 'movieDirector'})
+Director.belongsToMany(Movie,{through: 'directorMovie'})
+
 Movie.belongsToMany(Genre,{through: 'movieGenre'})
+Genre.belongsToMany(Movie,{through: 'genreMovie'})
